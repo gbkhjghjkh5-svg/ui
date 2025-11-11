2df43bc58b212fb4690d6841555043eb2f9a0ce9
gbkhjghjkh5-svg <gbkhjghjkh5@gmail.com>
2025-11-11 17:19:05 -0500

    Update Main.lua

--- Main.lua
+++ Main.lua
@@ -3,19 +3,10 @@ local RunService = game:GetService("RunService")
 local Players = game:GetService("Players")
 local UserInputService = game:GetService("UserInputService")
 local Camera = workspace.CurrentCamera
-local Services = Wrappers.Services
-setmetatable(Services, {
-	__index = function(self, Name: string)
-		local Service = game:GetService(Name)
-		return NewReference(Service)
-	end,
-})
 local CoreGui = Services.CoreGui
 local player = Players.LocalPlayer
-local gui = Instance.new("ScreenGui")
+local gui = Instance.new("ScreenGui", game:GetService("CoreGui").RobloxGui)
 gui.ResetOnSpawn = false
-gui.Parent = CoreGui:FindFirstChild("RobloxGui")
-
 local baseWidth, baseHeight = 400, 300
 local currentScale = 1
 
